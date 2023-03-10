905088e63c7f3e72b0afd905b220f71eada7ae16
Lawrence-tech <lawrencesagini2@gmail.com>
2023-03-09 21:57:08 -0800

    4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -13,9 +13,19 @@ class BaseModel:
         """ initializes the instance with three public instance attributes
             : id, created_at, and updated_at.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
+            self.id = kwargs.get('id', str(uuid.uuid4()))
+            self.created_at = kwargs.get('created_at', datetime.now())
+            self.updated_at = kwargs.get('updated_at', datetime.now())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ defined to return a string representation of the instance.
