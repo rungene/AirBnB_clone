e5d003ee84068b2538c8326e7f5e1b38ea43ee59
Lawrence-tech <lawrencesagini2@gmail.com>
2023-03-09 22:58:04 -0800

    example test base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 my_model = BaseModel()
 my_model.name = "My First Model"
