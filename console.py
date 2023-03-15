5efe9a57f0134271561075ec9f8b811bc0be50c7
rungene <rungenesamuel@gmail.com>
2023-03-14 19:25:52 -0700

    Adding classess and imports

--- console.py
+++ console.py
@@ -11,13 +11,17 @@ from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """ A class containig the command interpreter entry"""
     prompt = '(hbnb)'
 
-    list_class = ['BaseModel', 'User', 'State', 'City']
+    list_class = ['BaseModel', 'User', 'State', 'City', 'Amenity', 'Place',
+                  'Review']
 
     def do_EOF(self, args):
         """EOF to exit the program
