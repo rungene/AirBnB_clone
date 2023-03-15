51da0bbd208cba52e8d60159ae2994e3c6caa14e
rungene <rungenesamuel@gmail.com>
2023-03-14 18:33:30 -0700

    Adding user __init__ method

--- models/user.py
+++ models/user.py
@@ -4,9 +4,10 @@
 Created on Monday March 13 05:55:43 2023
 @authors: Clevers Rungene
           Lawrence
-user module         
+user module
 """
 from models.base_model import BaseModel
+
 
 class User(BaseModel):
     """class User that inherits from BaseModel
@@ -21,3 +22,13 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Init method for a class User
+
+        Attributes:
+            args(list): The list with arguments
+            kwargs(dict): A dictionary with arguments
+        """
+        super().__init__(*args, **kwargs)
