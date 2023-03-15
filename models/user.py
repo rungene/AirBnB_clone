c858b5c22effdd536d020e6d416552bb6126d7b6
rungene <rungenesamuel@gmail.com>
2023-03-14 18:22:47 -0700

    Write a class User that inherits from BaseModel:

---
+++ models/user.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Monday March 13 05:55:43 2023
+@authors: Clevers Rungene
+          Lawrence
+user module         
+"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """class User that inherits from BaseModel
+
+    Attributes:
+        email: string - empty string
+        password: string - empty string
+        first_name: string - empty string
+        last_name: string - empty string
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
